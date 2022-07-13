ccd0579bdd7e031496c3bf85731f20f27bc42b2c
ej8899 <ej8899@gmail.com>
2022-07-13 07:36:31 -0600

    work on template literal here

---
+++ assertEqual.js
@@ -0,0 +1,19 @@
+//
+// assertEqual(actual,expected) - compares via actual equal vs coercion & logs output to console only
+// part of lotide library.
+// 2022-07-13
+//
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`); // NOTE use of template literal here -- use back ticks to 'engage'
+  } else {
+    console.log("❌ Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+
+// MISC test calls
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+// assertEqual(true, true);
