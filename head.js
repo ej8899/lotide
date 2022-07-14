fbc364e8d764675cd758304d0a1fddb0040fbbaa
ej8899 <ej8899@gmail.com>
2022-07-13 19:44:52 -0600

    tail w modified assertEqual

--- head.js
+++ head.js
@@ -17,9 +17,13 @@ function head(inputArray) {
 }
 
 
+/*  
+MISC TESTS:
+
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 // extra testing below
 assertEqual(head("lobsters"), "lobsters");
 assertEqual(head(""), "");
 assertEqual(head(), undefined);
+*/
