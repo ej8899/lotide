e1b5ed599bc2c797db17673c45adfb7021fdd741
ej8899 <ej8899@gmail.com>
2022-07-15 19:42:30 -0600

    remove debug breaks

--- flatten.js
+++ flatten.js
@@ -11,7 +11,7 @@ function flatten(inputArray) {
 
   for (let x = 0;x <= inputArray.length-1; x++) {
     // if if item is array so we can do a 1st level recursion otherwise leave type as-is
-    console.log('FINAL so far:'+finalArray);
+    
     if(Array.isArray(inputArray[x])) {
       // recurse one level deep
       for (let y=0; y<= inputArray[x].length-1; y++) {
