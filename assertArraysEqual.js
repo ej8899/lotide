2d8e330ed267f181182e862437b76bad2b0e825a
ej8899 <ej8899@gmail.com>
2022-07-29 16:10:14 -0600

    refactor to use require & module.export

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,5 +1,7 @@
 
 
+const eqArrays = require('./eqArrays');
+
 function assertArraysEqual(arr1,arr2) {
   if (eqArrays(arr1,arr2) === true) {
     console.log(`✅ Assertion Passed: ${arr1} === ${arr2}`); // NOTE use of template literal here -- use back ticks to 'engage'
@@ -9,17 +11,5 @@ function assertArraysEqual(arr1,arr2) {
 }
 
 
-// input actual, expected
-function eqArrays(arr1,arr2) {
-  if(arr1.length !== arr2.length) {  // not even same length, lets avoid extra work
-    return false; 
-  }
-  for(let x=0; x <arr1.length; x++) {
-    if(arr1[x] !== arr2[x]) {
-      return false;
-    }
-  }
-  return true;
-}
 
 module.exports = assertArraysEqual;
