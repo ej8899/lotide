1e1edfcb1093f9fe4eaa29753d75f89d540f175d
ej8899 <ej8899@gmail.com>
2022-07-29 12:17:34 -0600

    minor (lint)

--- countOnly.js
+++ countOnly.js
@@ -16,7 +16,7 @@ const countOnly = function(allItems, itemsToCount) {
     // increment the counter for each item:
     //   set a property with that string key to:
     //     the value that was already there (or zero if nothing there) with 1 added to it.
- 
+
     if (itemsToCount[item]) { 
       if (results[item]) {
         results[item] += 1;
