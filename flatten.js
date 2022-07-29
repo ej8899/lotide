0ed8aaf428d001aa4ff8bba61e4ba644a20305ca
ej8899 <ej8899@gmail.com>
2022-07-29 16:40:43 -0600

    add module.exports

--- flatten.js
+++ flatten.js
@@ -1,16 +1,17 @@
 //
-// lotide project - flatten.js
+//  LHL - Lotide Library - flatten.js
+//  https://flex-web.compass.lighthouselabs.ca/workbooks/flex-m01w1/activities/186?journey_step=29&workbook=4
+//  v1.0 - 2022-07-18
 //
 
 // usage:
 // flatten([1, 2, [3, 4], 5, [6]]) //  EXPECTED OUTPUT: [1, 2, 3, 4, 5, 6]
 
-
 function flatten(inputArray) {
   let finalArray=[];
 
   for (let x = 0;x <= inputArray.length-1; x++) {
-    // if if item is array so we can do a 1st level recursion otherwise leave type as-is
+    // if item is array so we can do a 1st level recursion otherwise leave type as-is
     
     if(Array.isArray(inputArray[x])) {
       // recurse one level deep
@@ -29,10 +30,3 @@ assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]),[1, 2, 3, 4, 5, 6]);
 assertArraysEqual(flatten([1, 'a', [3, 4], 5, [6]]),[1, 'a', 3, 4, 5, 6]);
 assertArraysEqual(flatten([1, 'a', [3, 4], 5, []]),[1, 'a', 3, 4, 5]);
 */
-
-
-
-
-
-
-
